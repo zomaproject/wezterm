1629f87f748f18c8cc7db3599c18587c9b2ce0f7
zoma <zomaproject@gmail.com>
2022-06-20 02:15:22 -0500

    first commit

---
+++ wezterm.lua
@@ -0,0 +1,7 @@
+local wezterm = require 'wezterm';
+return {
+color_scheme = "OceanicMaterial",
+default_prog = {"C:/Program Files/PowerShell/7/pwsh.exe","-nologo"},
+  font = wezterm.font("Cascadia Code"),
+}
+
