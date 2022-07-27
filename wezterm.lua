acb837dc86dc4af7f6bd1ed7d3d92fb365e33e95
zoma <zotelolm@gmail.com>
2022-07-27 10:40:48 -0500

    update

--- wezterm.lua
+++ wezterm.lua
@@ -1,14 +1,14 @@
 local wezterm = require("wezterm")
 -- local catppuccin = require("colors/catppuccin").setup {}
 return {
-	default_cursor_style = "BlinkingBar",
-	color_scheme = "Gruvbox dark, hard (base16)",
-	default_prog = { "wsl", "~" },
+	-- default_cursor_style = "BlinkingBar",
+	color_scheme = "tokyonight-storm",
+	default_prog = {"C:/Program Files/Git/bin/bash"},
 	initial_cols = 100,
 	initial_rows = 30,
 
 	font = wezterm.font("MonoLisa"),
-	window_background_opacity = 0.8,
+	-- window_background_opacity = 0.8,
 	font_size = 11,
 	window_padding = {
 		left = 0,
@@ -21,12 +21,12 @@ return {
 		{ key = "J", mods = "CTRL|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Down", 5 } }) },
 		{ key = "K", mods = "CTRL|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Up", 5 } }) },
 		{ key = "L", mods = "CTRL|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Right", 5 } }) },
-		{ key = "h", mods = "CTRL", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
-		{ key = "Space", mods = "CTRL", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
-		{ key = "k", mods = "CTRL", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
-		{ key = "j", mods = "CTRL", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
+		{ key = "h", mods = "ALT", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
+		{ key = "l", mods = "ALT", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
+		{ key = "k", mods = "ALT", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
+		{ key = "j", mods = "ALT", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
 		{ key = "t", mods = "SHIFT|CTRL", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
-		{ key = "\\", mods = "CTRL", action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
+		{ key = "5", mods = "CTRL", action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
 		{ key = "Enter", mods = "CTRL|SHIFT", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
 	},
 	adjust_window_size_when_changing_font_size = false,
